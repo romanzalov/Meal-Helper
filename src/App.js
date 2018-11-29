43864190872a6b80b1ad66599531b28d717c2439
Roman <romanzalov@gmail.com>
2018-11-28 18:43:55 -0600

    testing out new editor

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       searchInput: '',
       recipes: [],
       anyRecipeFound: true,
-    }
+    };
     this.recipeCall = this.recipeCall.bind(this);
     this.handleInput = this.handleInput.bind(this);
   }
   handleInput(e) {
     this.setState({
       searchInput: e.target.value
-    })
+    });
     console.log(this.state.searchInput);
   }
 
   async recipeCall() {
     try {
-      const API_KEY = "0edbc557c96378697c37037a0e5b6fac"
+      const API_KEY = '0edbc557c96378697c37037a0e5b6fac';
       const response = await axios.get(`https://www.food2fork.com/api/search?key=${API_KEY}`);
-      console.log(response)
+      console.log(response);
       const recipes = response.data.recipes;
       this.setState({
         recipes: recipes,
@@ -40,7 +40,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.recipeCall();
+    this.recipeCall()
+        .then(data => {
+          console.log(data)
+        })
   }
   render() {
     return (
